3cffbcd9050fd4febb6214cbcfdbcb606e894ca3
JunHwan Kim <nefertiri972@gmail.com>
2022-04-26 01:49:51 +0900

    Add JDBC repository find
    - JDBC를 직접 사용한 리포지토리에서 Find 기능 구현

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,27 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        Member member = new Member("memberV4", 10000);
         repository.save(member);
+
+        Member foundMember = repository.findById(member.getMemberId());
+        log.info("foundMember={}", foundMember);
+        log.info("member == foundMember {}", member == foundMember);
+        log.info("member equals foundMember {}", member.equals(foundMember));
+        assertThat(foundMember).isEqualTo(member);
     }
 }
