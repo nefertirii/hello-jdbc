adfbb836ebcd9acf6caf0e962bce4a69e204d583
JunHwan Kim <nefertiri972@gmail.com>
2022-04-26 02:05:21 +0900

    Add JDBC repository update and delete
    - JDBC를 직접 사용한 리포지토리에서 Update, Delete 기능 구현

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,10 +2,13 @@ package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
@@ -15,13 +18,26 @@ class MemberRepositoryV0Test {
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV4", 10000);
+        // save
+        Member member = new Member("memberV100", 10000);
         repository.save(member);
 
+        // findById
         Member foundMember = repository.findById(member.getMemberId());
         log.info("foundMember={}", foundMember);
         log.info("member == foundMember {}", member == foundMember);
         log.info("member equals foundMember {}", member.equals(foundMember));
         assertThat(foundMember).isEqualTo(member);
+
+        // update
+        // money 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
