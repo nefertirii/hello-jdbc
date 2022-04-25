2a1a7bc35a46d1f927a05aef6dca3710fb40f37a
JunHwan Kim <nefertiri972@gmail.com>
2022-04-26 01:35:12 +0900

    Add JDBC repository save
    - JDBC를 직접 사용한 리포지토리에서 Save 기능 구현

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,69 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DbConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+/**
+ * JDBC - DriverManager 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member (member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+
+        // PreparedStatement는 Statement에 파라미터 바인딩이 추가됨
+        // SQL Injection을 방지하기 위해 파라미터 바인딩을 사용해야 함
+
+        if (rs != null) {
+            try { // Exception이 발생해도 아래 코드로 진행 가능
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DbConnectionUtil.getConnection();
+    }
+}
