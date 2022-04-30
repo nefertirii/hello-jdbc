4d5cb29ae9f757b0fd37cbafa5c0e4c7798e04bf
JunHwan Kim <nefertiri972@gmail.com>
2022-05-01 00:06:16 +0900

    Add spring exception translator to repository
    - 리포지토리에 스프링 예외 변환기 적용
    - 스프링이 예외를 추상화해주므로 서비스 계층은 특정 리포지토리의 구현 기술과 예외에 종속적이지 않게 된다.
    - 따라서 서비스 계층은 특정 구현 기술이 변경되어도 그대로 유지할 수 있게 된다.
    - 추가로 서비스 계층에서 예외를 잡아서 복구해야 하는 경우, 예외가 스프링이 제공하는 데이터 접근 예외로 변경되어서 서비스 계층에 넘어오기 때문에 필요한 경우 예외를 잡아서 복구하면 된다.

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -3,6 +3,7 @@ package hello.jdbc.service;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
@@ -48,7 +49,8 @@ class MemberServiceV4Test {
 
         @Bean
         MemberRepository memberRepositoryV4() {
-            return new MemberRepositoryV4_1(dataSource);
+            //return new MemberRepositoryV4_1(dataSource);
+            return new MemberRepositoryV4_2(dataSource);
         }
 
         @Bean
