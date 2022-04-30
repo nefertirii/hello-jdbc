663ebff272efbef932c44a1aae079ebe639e8265
JunHwan Kim <nefertiri972@gmail.com>
2022-04-30 11:12:18 +0900

    Add interface with runtime exception
    - 리포지토리에서 발생하는 체크 예외를 런타임 예외로 전환하여 밖으로 던진다. 이 떄 반드시 기존 예외를 포함하여 던져야한다.
    - 이제 리포지토리에서 런타임 예외만 던지므로 리포지토리 인터페이스에 예외를 명시하지 않아도된다.
    - 이제 서비스에서 리포지토리 인터페이스에만 의존하도록 수정한다.
    - 따라서 서비스는 특정 구현기술에 의존하지 않게된다.
    - 기존에는 서비스 계츠이 리포지토리에서 발생하는 체크 예외 떄문에 SQLException 이라는 JDBC 예외에 의존하였다.

--- src/test/java/hello/jdbc/service/MemberServiceV3_4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV3_4Test.java
@@ -1,8 +1,7 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
@@ -16,29 +15,27 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
- * 예외 누수 문제 해결
- *  * SQLException 제거
- *  *
- *  * MemberRepository 인터페이스 의존
+ * 트랜잭션 - DataSource, TransactionManager 자동 등록
  */
 
 @Slf4j
 @SpringBootTest
-class MemberServiceV4Test {
+class MemberServiceV3_4Test {
 
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
 
     @Autowired
-    private MemberRepository memberRepository;
+    private MemberRepositoryV3 memberRepository;
     @Autowired
-    private MemberServiceV4 memberService;
+    private MemberServiceV3_3 memberService;
 
     @RequiredArgsConstructor
     @TestConfiguration
@@ -47,18 +44,18 @@ class MemberServiceV4Test {
         private final DataSource dataSource;
 
         @Bean
-        MemberRepository memberRepositoryV4() {
-            return new MemberRepositoryV4_1(dataSource);
+        MemberRepositoryV3 memberRepositoryV3() {
+            return new MemberRepositoryV3(dataSource);
         }
 
         @Bean
-        MemberServiceV4 memberServiceV4() {
-            return new MemberServiceV4(memberRepositoryV4());
+        MemberServiceV3_3 memberServiceV3_3() {
+            return new MemberServiceV3_3(memberRepositoryV3());
         }
     }
 
     @AfterEach
-    void after() {
+    void after() throws SQLException {
         memberRepository.delete(MEMBER_A);
         memberRepository.delete(MEMBER_B);
         memberRepository.delete(MEMBER_EX);
@@ -66,7 +63,7 @@ class MemberServiceV4Test {
 
     @Test
     @DisplayName("정상 이체")
-    void accountTransfer() {
+    void accountTransfer() throws SQLException {
         // given
         Member memberA = new Member(MEMBER_A, 10000);
         Member memberB = new Member(MEMBER_B, 10000);
@@ -87,7 +84,7 @@ class MemberServiceV4Test {
 
     @Test
     @DisplayName("이체중 예외 발생")
-    void accountTransferEx() {
+    void accountTransferEx() throws SQLException {
         // given
         Member memberA = new Member(MEMBER_A, 10000);
         Member memberEx = new Member(MEMBER_EX, 10000);
